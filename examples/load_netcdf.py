eeec1ead921f5b57625110f3db35b0f5abc44ad2
chrishavlin <chris.havlin@gmail.com>
2021-09-17 17:06:01 -0500

    unset log

--- examples/load_netcdf.py
+++ examples/load_netcdf.py
@@ -8,7 +8,7 @@ ds = XarrayGeoSpherical(filename)
 x, y, z = ds.cartesian_coords()
 
 ds_yt = XarrayGeoSpherical(filename).load_uniform_grid()
-
-
 slc = yt.SlicePlot(ds_yt, "depth", "dvs")
+slc.set_log("dvs", False)
+slc.set_zlim("dvs", -3, 3)
 slc.save()
