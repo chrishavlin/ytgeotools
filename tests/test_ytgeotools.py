03eb542ad139d0367e358f0c842eadcf2fec4ee4
chrishavlin <chris.havlin@gmail.com>
2021-10-19 12:33:04 -0500

    testing updates

--- tests/test_ytgeotools.py
+++ tests/test_ytgeotools.py
@@ -4,21 +4,52 @@
 
 import pytest
 
-
+import numpy as np
 from ytgeotools import ytgeotools
+import yt
+
+def get_dataset(shp=(2, 3, 4), fields=("density",), cnms=("x", "y", "z")):
+    shp = (2, 3, 4)
+
+    data = {f: np.random.random(shp) for f in fields}
+    coords = {}
+    for cid, c in enumerate(cnms):
+        coords[cid] = {"values":  np.linspace(0, 1, shp[cid]), "name": c}
+
+    return ytgeotools.Dataset(data, coords)
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def check_field(ds, field, fshape):
+    assert field in ds.fields
+    assert hasattr(ds, field)
+    fvar = getattr(ds, field)
+    assert fvar.shape == fshape
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_dataset():
+    # simple tests of instantiation and functionality of base dataset
+    test_fields = ("test1", "test2")
+    fshp = (2, 3, 4)
+    cnms = ("x", "y", "z")
+    ds = get_dataset(shp=fshp, fields=test_fields, cnms=cnms)
+
+    for fld in test_fields:
+        check_field(ds, fld, fshp)
+
+    assert ds._coord_order == [c for c in cnms]
+    for cid, c in enumerate(cnms):
+        ds.get_coord(c).shape == (fshp[cid], )
+
+
+def test_to_yt():
+    # simple tests of instantiation and functionality of base dataset
+    test_fields = ("test1", "test2")
+    fshp = (2, 3, 4)
+    cnms = ("x", "y", "z")
+    ds = get_dataset(shp=fshp, fields=test_fields, cnms=cnms)
+
+    yt_ds = ds.load_uniform_grid()
+    for f in test_fields:
+        assert ("stream", f) in yt_ds.field_list
+        assert type(yt_ds) == yt.frontends.stream.StreamDataset
+
